3f5967d02000780f569404fc232bad793dbd2328
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-18 15:02:54 +0000

    comments

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
+  # Check if month is invalid
   if month > 12 or month < 1:
     return "Invalid month"
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
